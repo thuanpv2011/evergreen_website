32714b28c594578e1a44a27c65e49104dd9e4ca8
thuanpv1 <thuanpv@egt.vn>
2019-08-17 18:05:40 +0700

    Updates

---
+++ precache-manifest.02cd7d27d862064ad644af9b52f30a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3c9d6ea12499742832",
+    "url": "/evergreen_website/static/js/runtime~main.4d3c9d6e.js"
+  },
+  {
+    "revision": "7a5f7d044c1a50fdbbab",
+    "url": "/evergreen_website/static/js/main.7a5f7d04.chunk.js"
+  },
+  {
+    "revision": "851a3ccba2a82b2ff4aa",
+    "url": "/evergreen_website/static/js/2.851a3ccb.chunk.js"
+  },
+  {
+    "revision": "7a5f7d044c1a50fdbbab",
+    "url": "/evergreen_website/static/css/main.88b0e87b.chunk.css"
+  },
+  {
+    "revision": "1f6af4a8d5733b8d0228a5eedcdfb643",
+    "url": "/evergreen_website/index.html"
+  }
+];
