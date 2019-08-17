48479ba8fb14e452ac5bc56111f471b9e04c7dab
thuanpv1 <thuanpv@egt.vn>
2019-08-17 18:10:59 +0700

    add new page

---
+++ src/components/Home/auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AuthHome() {
+    return (
+        <>
+            <h1>EGT-Home</h1>
+            <p>You are logged in</p>
+        </>
+    )
+}
+
+export default AuthHome;
